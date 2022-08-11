2bc37f3cb026b2134ed15f7bd3ba568b3c719755
Gabik21 <gabik.fabian@gmail.com>
2022-08-11 12:02:04 +0200

    Add /whitelist list and don't use slow ass minecraftapi.net
    Regular mojang is 2 seconds faster...

--- src/main/java/com/james090500/VelocityWhitelist/commands/CommandBuilder.java
+++ src/main/java/com/james090500/VelocityWhitelist/commands/CommandBuilder.java
@@ -11,55 +11,65 @@ import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
-
 import java.util.concurrent.CompletableFuture;
 
 public class CommandBuilder {
 
-    private static ProxyServer server;
+  private static ProxyServer server;
 
-    /**
-     * Reginster all commands
-     * @param velocityWhitelist
-     */
-    public static void register(VelocityWhitelist velocityWhitelist) {
-        server = velocityWhitelist.getServer();
-        //Setup command flow
-        final CommandHandler handler = new CommandHandler(velocityWhitelist);
-        server.getCommandManager().register(server.getCommandManager().metaBuilder("vwhitelist").build(), new BrigadierCommand(
-                LiteralArgumentBuilder.<CommandSource>literal("vwhitelist").requires(sender -> sender.hasPermission("vwhitelist.admin")).executes(handler::about)
-                        .then(LiteralArgumentBuilder.<CommandSource>literal("on").executes(handler::turnOn))
-                        .then(LiteralArgumentBuilder.<CommandSource>literal("off").executes(handler::turnOff))
+  /**
+   * Reginster all commands
+   */
+  public static void register(VelocityWhitelist velocityWhitelist) {
+    server = velocityWhitelist.getServer();
+    //Setup command flow
+    final CommandHandler handler = new CommandHandler(velocityWhitelist);
+    server.getCommandManager()
+        .register(server.getCommandManager().metaBuilder("vwhitelist").build(),
+            new BrigadierCommand(
+                LiteralArgumentBuilder.<CommandSource>literal("vwhitelist")
+                    .requires(sender -> sender.hasPermission("vwhitelist.admin"))
+                    .executes(handler::about)
+                    .then(LiteralArgumentBuilder.<CommandSource>literal("on")
+                        .executes(handler::turnOn))
+                    .then(LiteralArgumentBuilder.<CommandSource>literal("off")
+                        .executes(handler::turnOff))
 
-                        .then(LiteralArgumentBuilder.<CommandSource>literal("add").executes(handler::add))
-                        .then(LiteralArgumentBuilder.<CommandSource>literal("add")
-                                .then(RequiredArgumentBuilder.<CommandSource, String>argument("username", StringArgumentType.word())
-                                .suggests(CommandBuilder::allPlayers)
-                                .executes(handler::add)))
+                    .then(
+                        LiteralArgumentBuilder.<CommandSource>literal("add").executes(handler::add))
+                    .then(LiteralArgumentBuilder.<CommandSource>literal("add")
+                        .then(RequiredArgumentBuilder.<CommandSource, String>argument("username",
+                                StringArgumentType.word())
+                            .suggests(CommandBuilder::allPlayers)
+                            .executes(handler::add)))
 
-                        .then(LiteralArgumentBuilder.<CommandSource>literal("remove").executes(handler::remove))
-                        .then(LiteralArgumentBuilder.<CommandSource>literal("remove")
-                                .then(RequiredArgumentBuilder.<CommandSource, String>argument("username", StringArgumentType.word())
-                                .suggests(CommandBuilder::allPlayers)
-                                .executes(handler::remove)))
+                    .then(LiteralArgumentBuilder.<CommandSource>literal("remove")
+                        .executes(handler::remove))
+                    .then(LiteralArgumentBuilder.<CommandSource>literal("remove")
+                        .then(RequiredArgumentBuilder.<CommandSource, String>argument("username",
+                                StringArgumentType.word())
+                            .suggests(CommandBuilder::allPlayers)
+                            .executes(handler::remove)))
+                    .then(LiteralArgumentBuilder.<CommandSource>literal("list")
+                        .executes(handler::list))
+                    .then(LiteralArgumentBuilder.<CommandSource>literal("reload")
+                        .requires(source -> source.hasPermission("vgui.admin"))
+                        .executes(handler::reload))
+            ));
+  }
 
-                        .then(LiteralArgumentBuilder.<CommandSource>literal("reload").requires(source -> source.hasPermission("vgui.admin")).executes(handler::reload))
-        ));
+  /**
+   * An all players tab suggestions
+   */
+  private static CompletableFuture<Suggestions> allPlayers(CommandContext<CommandSource> context,
+      SuggestionsBuilder builder) {
+    for (Player player : server.getAllPlayers()) {
+      String username = player.getUsername();
+      if (username.toLowerCase().startsWith(context.getInput().toLowerCase())
+          || username.equalsIgnoreCase(context.getInput())) {
+        builder.suggest(username);
+      }
     }
-
-    /**
-     * An all players tab suggestions
-     * @param context
-     * @param builder
-     * @return
-     */
-    private static CompletableFuture<Suggestions> allPlayers(CommandContext<CommandSource> context, SuggestionsBuilder builder) {
-        for (Player player : server.getAllPlayers()) {
-            String username = player.getUsername();
-            if (username.toLowerCase().startsWith(context.getInput().toLowerCase()) || username.equalsIgnoreCase(context.getInput())) {
-                builder.suggest(username);
-            }
-        }
-        return builder.buildFuture();
-    }
+    return builder.buildFuture();
+  }
 }
--- src/main/java/com/james090500/VelocityWhitelist/helpers/MinecraftApi.java
+++ src/main/java/com/james090500/VelocityWhitelist/helpers/MinecraftApi.java
@@ -10,6 +10,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public record MinecraftApi(VelocityWhitelist velocityWhitelist) {
@@ -21,55 +24,7 @@ public record MinecraftApi(VelocityWhitelist velocityWhitelist) {
      * @return Players uuid
      */
     public UUID getUUID(String username) {
-        //Try get online player first
-        if (velocityWhitelist.getServer().getPlayer(username).isPresent()) {
-            return velocityWhitelist.getServer().getPlayer(username).get().getUniqueId();
-        }
-
-        JsonObject playerElement = getApiData(username);
-        if (playerElement != null) {
-            JsonElement playerUUID = playerElement.get("full_uuid");
-            if (playerUUID != null && !playerUUID.isJsonNull()) {
-                return UUID.fromString(playerUUID.getAsString());
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Request API call for user data
-     *
-     * @param data The username/uuid to send
-     * @return The response data
-     */
-    private static JsonObject getApiData(String data) {
-        try {
-            URL url = new URL("https://minecraftapi.net/api/v1/profile/" + data);
-            HttpURLConnection httpurlconnection = (HttpURLConnection) url.openConnection();
-            httpurlconnection.setDoInput(true);
-            httpurlconnection.setDoOutput(false);
-            httpurlconnection.connect();
-
-            if (httpurlconnection.getResponseCode() / 100 == 2) {
-                //Create reader
-                BufferedReader in = new BufferedReader(new InputStreamReader(httpurlconnection.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-
-                //Read response
-                while ((inputLine = in.readLine()) != null)
-                    response.append(inputLine);
-
-                //Convert response to JSON
-                return JsonParser.parseString(response.toString()).getAsJsonObject();
-            } else {
-                return null;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+        return UUIDFetcher.getUUID(username);
     }
 
 }
